3336d3991e5b2e33bb73bbad298ca79e0661ac90
Frank Brehm <frank@brehm-online.com>
2021-09-29 17:25:38 +0200

    Changing license to LGPL-3

--- lib/fb_logging/__init__.py
+++ lib/fb_logging/__init__.py
@@ -11,7 +11,7 @@ __author__ = 'Frank Brehm <frank@brehm-online.com>'
 __copyright__ = '(C) 2021 by Frank Brehm, Berlin'
 __contact__ = 'frank@brehm-online.com'
 __version__ = '0.2.2'
-__license__ = 'AGPL'
+__license__ = 'LGPL-3'
 
 # Standard modules
 import os
