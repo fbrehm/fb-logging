53657ce9a9368ca73d4d6a89f8b5367b433dc350
Frank Brehm <frank@brehm-online.com>
2025-09-16 14:43:39 +0200

    Finishing src/fb_logging/dch2spl.py

--- src/fb_logging/dch2spl.py
+++ src/fb_logging/dch2spl.py
@@ -10,12 +10,15 @@
 from __future__ import print_function
 
 # Standard modules
+import datetime
 import logging
 import os
 import platform
 import pprint
 import re
 import sys
+import textwrap
+import warnings
 from pathlib import Path
 
 # 3rd party modules
@@ -24,10 +27,11 @@ import click
 # Own modules
 from fb_logging import __version__ as __pkg_version__
 from fb_logging.colored import ColoredFormatter
+from fb_logging.deb_changelog import Changelog
 
 # from fb_logging.deb_changelog import Changelog
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 LOG = logging.getLogger(__name__)
 
@@ -51,6 +55,13 @@ class Dch2SpecLogEnv(object):
 
     Converts a Debian changelog into log entries of a RPM spec file.
     """
+
+    re_emptyline = re.compile(r"^\s*$")
+    re_start_line = re.compile(r"^  \* (.*)")
+    re_next_line = re.compile(r"^    (.*)")
+    re_day_str = re.compile(r"\s+\d\d:\d\d:\d\d\s+[+-]?\d{4}$")
+
+    output_line_width = 70
 
     # -------------------------------------------------------------------------
     @classmethod
@@ -129,8 +140,14 @@ class Dch2SpecLogEnv(object):
         return has_colors
 
     # -------------------------------------------------------------------------
-    def __init__(self, appname=None, verbose=0, version=__pkg_version__, has_colors=None):
+    def __init__(
+        self, changelog_file, appname=None, verbose=0, version=__pkg_version__, has_colors=None
+    ):
         """Initialise the application object."""
+        if changelog_file is None:
+            self.changelog_file = None
+        else:
+            self.changelog_file = Path(changelog_file)
         if appname:
             self.appname = self.get_generic_appname()
         else:
@@ -144,7 +161,8 @@ class Dch2SpecLogEnv(object):
 
         self.init_logging()
 
-        LOG.debug(f"Parameter 'has_colors' was {has_colors!r}.")
+        if self.verbose > 1:
+            LOG.debug(f"Parameter 'has_colors' was {has_colors!r}.")
 
     # -------------------------------------------------------------------------
     def __repr__(self):
@@ -152,7 +170,12 @@ class Dch2SpecLogEnv(object):
         out = "<%s(" % (self.__class__.__name__)
 
         fields = []
-        fields.append(f"appname=={self.appname!r}")
+        cfile = None
+        if self.changelog_file is not None:
+            cfile = str(self.changelog_file)
+
+        fields.append(f"changelog_file={cfile!r}")
+        fields.append(f"appname={self.appname!r}")
         fields.append(f"verbose={self.verbose!r}")
         fields.append(f"version={self.version!r}")
         fields.append(f"has_colors={self.has_colors!r}")
@@ -218,8 +241,129 @@ class Dch2SpecLogEnv(object):
 
         return
 
+    # -------------------------------------------------------------------------
+    def __call__(self):
+        """
+        Execute the main action of converting.
 
-# pass_environment = click.make_pass_decorator(Dch2SpecLogEnv, ensure=True)
+        Makes the application object callable.
+        """
+        if self.changelog_file:
+            cfile = str(self.changelog_file)
+            LOG.debug(f"Reading {cfile!r} ...")
+            with self.changelog_file.open("r", encoding="utf-8", errors="backslashreplace") as fh:
+                self.convert(fh, str(self.changelog_file))
+        else:
+            self.convert(sys.stdin, "STDIN")
+
+    # -------------------------------------------------------------------------
+    def mangle_changes(self, changes):
+        """Transform the changes into the RPM chngelog format."""
+        clist = []
+        change = None
+
+        for line in changes:
+
+            if self.re_emptyline.match(line):
+                continue
+
+            m = self.re_start_line.match(line)
+            if m:
+                if change:
+                    clist.append(change)
+                change = m.group(1)
+                continue
+
+            m = self.re_next_line.match(line)
+            if m:
+                if change:
+                    change += " " + m.group(1)
+                else:
+                    change = m.group(1)
+                continue
+
+            warnings.warn(
+                "Could not evaluate Changelog entry {!r}.".format(line),
+                SyntaxWarning,
+                stacklevel=1,
+            )
+
+        if change:
+            clist.append(change)
+
+        return clist
+
+    # -------------------------------------------------------------------------
+    def convert(self, fh, filename):
+        """Transform the complete contenet of the given changelog file into RPM changelog files."""
+        ch = None
+
+        with warnings.catch_warnings(record=True) as w:
+            warnings.simplefilter("always")
+            ch = Changelog(fh)
+
+            if len(w):
+                msg = "There were {nr} warnings on reading {f!r}.".format(nr=len(w), f=filename)
+                LOG.warning(msg)
+                for msg in w:
+                    category = msg.category.__name__
+                    s = f"{category}: {msg.message}\n"
+                    LOG.warning(s)
+                sys.exit(5)
+
+        LOG.debug("Changelog {f!r} has {nr} entries.".format(f=filename, nr=len(ch)))
+
+        days = {}
+
+        for block in ch:
+
+            lines = []
+            day_str = self.re_day_str.sub("", block.date)
+            date = datetime.datetime.strptime(day_str, "%a, %d %b %Y")
+
+            day = date.strftime("%Y-%m-%d")
+            author = block.author
+            version = str(block.version)
+            if not block.version.debian_revision:
+                version += "-1"
+            lines.append(
+                "*   {date} {author} {version}".format(date=date, author=author, version=version)
+            )
+
+            changes = self.mangle_changes(block._changes)
+
+            if day not in days:
+                days[day] = {
+                    "date": date.strftime("%a %b %d %Y"),
+                    "author": author,
+                    "version": version,
+                    "changes": changes,
+                }
+            else:
+                for change in changes:
+                    days[day]["changes"].append(change)
+
+        for day in sorted(days.keys(), reverse=True):
+            lines = []
+            block = days[day]
+            lines.append(
+                "*   {date} {author} {version}".format(
+                    date=block["date"], author=block["author"], version=block["version"]
+                )
+            )
+
+            for change in block["changes"]:
+                for line in textwrap.wrap(
+                    change, width=self.output_line_width, initial_indent="-   ",
+                    subsequent_indent="    "
+                ):
+                    lines.append(line)
+
+            click.echo("\n".join(lines))
+
+
+# =============================================================================
+
 CONTEXT_SETTINGS = {"help_option_names": ["-h", "--help"]}
 
 
@@ -227,26 +371,30 @@ CONTEXT_SETTINGS = {"help_option_names": ["-h", "--help"]}
 @click.argument(
     "changelog_file",
     type=click.Path(exists=True, file_okay=True, dir_okay=True, readable=True, path_type=Path),
-    help=("The Debian changelog file to convert into log entries of a RPM spec file."),
+    required=False,
 )
 @click.option(
-    "--color/--no-color", "has_color", default=None, help="se colored output for messages."
+    "--color/--no-color", "has_color", default=None, help="Set colored output for messages."
 )
 @click.option(
     "-v", "--verbose", count=True, type=click.IntRange(0, 5), help="Increase the verbosity level."
 )
 @click.version_option()
-# @pass_environment
 @click.pass_context
-def main(ctx, has_color, verbose, input_file):
-    """Convert a Debian changelog into log entries of a RPM spec file."""
-    ctx.obj = Dch2SpecLogEnv(verbose=verbose, has_colors=has_color)
+def main(ctx, changelog_file, has_color, verbose):
+    """
+    Convert a Debian CHANGELOG_FILE into log entries of a RPM spec file.
+
+    If CHANGELOG_FILE is omitted, then the input will be read from STDIN.
+    """
+    ctx.obj = Dch2SpecLogEnv(changelog_file, verbose=verbose, has_colors=has_color)
 
     if verbose > 2:
-        print(
+        click.echo(
             "{c}-Object:\n{a}".format(c=ctx.__class__.__name__, a=pp(ctx.__dict__)),
             file=sys.stderr,
         )
 
+    ctx.obj()
 
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
