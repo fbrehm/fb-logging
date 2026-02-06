0ae590c0d0a1ce160749a17d18246cd112ed5eab
Frank Brehm <frank@brehm-online.com>
2026-02-06 12:00:18 +0100

    Version dump to 1.3.8 and updating CHANGELOG.md

--- src/fb_logging/__init__.py
+++ src/fb_logging/__init__.py
@@ -5,13 +5,13 @@
 
 @author: Frank Brehm
 @contact: frank@brehm-online.com
-@copyright: © 2025 by Frank Brehm, Berlin
+@copyright: © 2026 by Frank Brehm, Berlin
 """
 
 __author__ = "Frank Brehm <frank@brehm-online.com>"
-__copyright__ = "(C) 2025 by Frank Brehm, Berlin"
+__copyright__ = "(C) 2026 by Frank Brehm, Berlin"
 __contact__ = "frank@brehm-online.com"
-__version__ = "1.3.7"
+__version__ = "1.3.8"
 __license__ = "LGPL-3"
 
 # Standard modules
